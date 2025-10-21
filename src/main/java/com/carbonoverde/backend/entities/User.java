6d5f279b42fcfde3cce5db6d12306ab85530621d
alexritzmann <alexritzmann@gmail.com>
2025-10-20 21:30:49 -0300

    Start creation of entities

---
+++ src/main/java/com/carbonoverde/backend/entities/User.java
@@ -0,0 +1,67 @@
+package com.carbonoverde.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @NotBlank
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @NotBlank
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+
+}
